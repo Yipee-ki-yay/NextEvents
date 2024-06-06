fdd660e46255f8cbc8354904b323cbd024b6e635
Oleksandr Morozov <mopoz07@gmail.com>
2024-06-06 18:13:21 +0300

    implement NextEvents app

---
+++ components/events/event-item.js
@@ -0,0 +1,43 @@
+import classes from "./event-item.module.css";
+import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
+
+function EventItem({ title, image, date, location, id }) {
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  const formatedAddress = location.replace(", ", "\n");
+  const exploreLink = `/events/${id}`;
+
+  return (
+    <li className={classes.item}>
+      <img src={"/" + image} alt={title} />
+      <div className={classes.content}>
+        <h2>{title}</h2>
+        <div className={classes.date}>
+          <DateIcon />
+          <time>{humanReadableDate}</time>
+        </div>
+        <div className={classes.address}>
+          <AddressIcon />
+          <address>{formatedAddress}</address>
+        </div>
+      </div>
+
+      <div className={classes.actions}>
+        <Button link={exploreLink}>
+          <span>Explore Event</span>
+          <span className={classes.icon}>
+            <ArrowRightIcon />
+          </span>
+        </Button>
+      </div>
+    </li>
+  );
+}
+
+export default EventItem;
